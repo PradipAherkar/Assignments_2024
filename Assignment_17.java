a4ed057680269b7abed8060685cd356b8336c037
PradipAherkar <pradeep99aherkar@gmail.com>
2024-11-24 23:07:37 +0530

    Comparison for case-sensitive and case-insensitive equality

--- Assignment_17.java
+++ Assignment_17.java
@@ -2,9 +2,9 @@
 import java.util.Scanner;
 
 public class Assignment_17 {
-    public static void main(String[] args) {
 
-        // Reverse a string without using built-in functions
+    static void task1(){
+            // Reverse a string without using built-in functions
 
         Scanner s=new Scanner(System.in);
 
@@ -19,30 +19,43 @@ public class Assignment_17 {
         
                 System.out.println("Original String: " + original);
                 System.out.println("Reversed String: " + reversed);       
-                
-      // Count the number of vowels, consonants, digits, and spaces in a string
+    }
 
-            String str = "Java Programming 101!";
-            int vowels = 0, consonants = 0, digits = 0, spaces = 0;
-    
-            for (char ch : str.toCharArray()) {
-                if (Character.isDigit(ch)) {
-                    digits++;
-                } else if (Character.isLetter(ch)) {
-                    if ("AEIOUaeiou".indexOf(ch) != -1) {
-                        vowels++;
-                    } else {
-                        consonants++;
-                    }
-                } else if (Character.isWhitespace(ch)) {
-                    spaces++;
-                }
-            }
-    
-            System.out.println("Vowels: " + vowels);
-            System.out.println("Consonants: " + consonants);
-            System.out.println("Digits: " + digits);
-            System.out.println("Spaces: " + spaces);
+    static void task2(){      
+              // Count the number of vowels, consonants, digits, and spaces in a string
+              String str = "Java Programming 101!";
+              int vowels = 0, consonants = 0, digits = 0, spaces = 0;
+      
+              for (char ch : str.toCharArray()) {
+                  if (Character.isDigit(ch)) {
+                      digits++;
+                  } else if (Character.isLetter(ch)) {
+                      if ("AEIOUaeiou".indexOf(ch) != -1) {
+                          vowels++;
+                      } else {
+                          consonants++;
+                      }
+                  } else if (Character.isWhitespace(ch)) {
+                      spaces++;
+                  }
+              }
+      
+              System.out.println("Vowels: " + vowels);
+              System.out.println("Consonants: " + consonants);
+              System.out.println("Digits: " + digits);
+              System.out.println("Spaces: " + spaces);
+    }
 
+    static void task3(){
+        // Compare two strings for equality (case-sensitive and case-insensitive)
+                String str1 = "Hello";
+                String str2 = "hello";
+                System.out.println("Case-Sensitive Comparison: " + str1.equals(str2));
+                System.out.println("Case-Insensitive Comparison: " + str1.equalsIgnoreCase(str2));   
+    }
+
+    public static void main(String[] args) {
+        task1();
+        task2();
     }
 }
