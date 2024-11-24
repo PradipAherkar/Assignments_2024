e6b77f87e775b97c02ecf5f9c05664413cbc3243
PradipAherkar <pradeep99aherkar@gmail.com>
2024-11-24 23:00:52 +0530

    Count the number of vowels, consonants, digits, and spaces in a string

--- Assignment_17.java
+++ Assignment_17.java
@@ -3,6 +3,9 @@ import java.util.Scanner;
 
 public class Assignment_17 {
     public static void main(String[] args) {
+
+        // Reverse a string without using built-in functions
+
         Scanner s=new Scanner(System.in);
 
         System.out.println("Enter The String : ");
@@ -15,6 +18,31 @@ public class Assignment_17 {
                 }
         
                 System.out.println("Original String: " + original);
-                System.out.println("Reversed String: " + reversed);        
+                System.out.println("Reversed String: " + reversed);       
+                
+      // Count the number of vowels, consonants, digits, and spaces in a string
+
+            String str = "Java Programming 101!";
+            int vowels = 0, consonants = 0, digits = 0, spaces = 0;
+    
+            for (char ch : str.toCharArray()) {
+                if (Character.isDigit(ch)) {
+                    digits++;
+                } else if (Character.isLetter(ch)) {
+                    if ("AEIOUaeiou".indexOf(ch) != -1) {
+                        vowels++;
+                    } else {
+                        consonants++;
+                    }
+                } else if (Character.isWhitespace(ch)) {
+                    spaces++;
+                }
+            }
+    
+            System.out.println("Vowels: " + vowels);
+            System.out.println("Consonants: " + consonants);
+            System.out.println("Digits: " + digits);
+            System.out.println("Spaces: " + spaces);
+
     }
 }
