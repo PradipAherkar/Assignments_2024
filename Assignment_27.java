40966ff0883b51b265e44cb2263a6f2c74e162d2
PradipAherkar <pradeep99aherkar@gmail.com>
2024-11-28 21:11:38 +0530

    Program to check number is power of 2 or not..

--- Assignment_27.java
+++ Assignment_27.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 
 public class Assignment_27{
 
-static void task1(){
+        static void task1(){
              // Program to count the number of 1 bits in an integer using bitwise operations
              System.out.println("Enter any number to check 1 bits in it.");
              Scanner s=new Scanner(System.in);
@@ -14,27 +14,44 @@ static void task1(){
              while (num != 0) {
                  count += (num & 1);
                  num >>= 1; // Right shift
-                }
-                
-                System.out.println("Number of 1 bits: " + count);
             }
+                
+            System.out.println("Number of 1 bits: " + count);
+        }
             
-            static void task2(){
+        static void task2(){
              // Program to check if a number is even or odd using the bitwise AND operator
              System.out.println("Enter any number to check even or odd.");
              Scanner s=new Scanner(System.in);
              int num = s.nextInt();
-
+             
              // Check if the number is even or odd using bitwise AND
              if ((num & 1) == 0) {
-                System.out.println(num + " is even.");
-             } else {
-                System.out.println(num + " is odd.");
+                 System.out.println(num + " is even.");
+                } else {
+                    System.out.println(num + " is odd.");
+                }
             }
-}
+            
+        static void task3(){
+            // Program to check if a number is a power of 2 using bitwise ANDint num = 16;
+                
+            System.out.println("Enter any number to check it is power of 2 or not.");
+            Scanner s=new Scanner(System.in);
+            int num = s.nextInt();
 
-    public static void main(String[] args) {
-        task1();
-        task2();
+            // Check if the number is a power of 2 using bitwise AND
+
+            if (num > 0 && (num & (num - 1)) == 0) {
+                System.out.println(num + " is a power of 2.");
+            } else {
+                System.out.println(num + " is not a power of 2.");
+            }
+        }
+            
+        public static void main(String[] args) {
+            task1();
+            task2();
+            task3();
     }
 }
